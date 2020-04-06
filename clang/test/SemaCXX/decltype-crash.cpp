9eab944c22213e687b963a7a478571d6686c57dd
Haojian Wu <hokein.wu@gmail.com>
2020-04-06 09:02:52 +0200

    [AST] Adjust existing tests for recovery-exprs
    Summary:
    When enabling recovery-expr (in https://reviews.llvm.org/D76696, which has been
    revertedj), we need to update a few tests to adjust new diagnostic changes.
    
    This patch updates all relevant tests, it will make the developing life
    easier (if recovery-expr is enabled by default, we can revert this
    patch).
    
    Reviewers: sammccall, jdoerfert
    
    Subscribers: cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D77527

--- clang/test/SemaCXX/decltype-crash.cpp
+++ clang/test/SemaCXX/decltype-crash.cpp
@@ -1,7 +1,10 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -Wc++11-compat -std=c++98 %s
+// RUN: %clang_cc1 -fsyntax-only -frecovery-ast -verify -Wc++11-compat -std=c++98 %s
 
 int& a();
 
 void f() {
-  decltype(a()) c; // expected-warning {{'decltype' is a keyword in C++11}} expected-error {{use of undeclared identifier 'decltype'}}
+  decltype(a()) c; // expected-warning {{'decltype' is a keyword in C++11}} \
+                   // expected-error {{use of undeclared identifier 'decltype'}} \
+                   // expected-error {{expected ';' after expression}} \
+                   // expected-error {{use of undeclared identifier 'c'}}
 }
