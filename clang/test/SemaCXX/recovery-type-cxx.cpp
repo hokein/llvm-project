ed3cd31274b545460d53dbe512b0325f1b4c1301
Haojian Wu <hokein.wu@gmail.com>
2020-04-28 13:35:25 +0200

    Fix invalid-member tests.

--- clang/test/SemaCXX/recovery-type-cxx.cpp
+++ clang/test/SemaCXX/recovery-type-cxx.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple=x86_64-unknown-unknown -frecovery-ast -frecovery-ast-type -std=c++11 -o - %s -fsyntax-only -verify
+// RUN: %clang_cc1 -triple=x86_64-unknown-unknown -frecovery-ast -frecovery-ast-type -o - %s -fsyntax-only -verify
 
 namespace NoCrash{
 struct Indestructible {
@@ -11,4 +11,12 @@ Indestructible make_indestructible();
 void test() {
   int s = sizeof(make_indestructible()); // expected-error {{deleted}}
 }
+}
+
+namespace {
+void foo(); // expected-note {{requires 0 arguments}}
+class Y {
+  // no "field has incomplete type" diagnostic.
+  typeof(foo(42)) invalid; // expected-error {{no matching function}}
+};
 }
