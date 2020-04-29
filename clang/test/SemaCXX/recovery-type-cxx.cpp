eee5348fd6ed9a0b7853ac3e6d07abd641eb0e4d
Haojian Wu <hokein.wu@gmail.com>
2020-04-29 10:09:40 +0200

    Adjust the enable_if test.
    The diagnostic is changed a little bit for this (even with
    -fno-recovery-ast-type), but I think it is fine.

--- clang/test/SemaCXX/recovery-type-cxx.cpp
+++ clang/test/SemaCXX/recovery-type-cxx.cpp
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -triple=x86_64-unknown-unknown -frecovery-ast -frecovery-ast-type -o - %s -fsyntax-only -verify
 
-namespace NoCrash{
+namespace NoCrash {
 struct Indestructible {
   // Indestructible();
   ~Indestructible() = delete; // expected-note {{deleted}}
@@ -40,4 +40,19 @@ void test() { // expected-note {{candidate function}}
   // FIXME: suppress the init_conversion_failed diagnostic.
   int a = test(1); // expected-error {{no matching function}} expected-error {{cannot initialize a variable of type}}
 }
+}
+
+namespace enable_if_diags {
+template <int N> constexpr int templated() __attribute__((enable_if(N, ""))) {
+  return 1;
+}
+
+template <int N> constexpr int callTemplated() {
+  return templated<N>();  // expected-error {{no matching function for call to 'templated'}} 
+                          // expected-note@+5 {{in instantiation of function template specialization}}
+                          // expected-note@-7 {{candidate disabled}}
+}
+
+constexpr int B = 10 +  // expected-error {{constexpr variable 'B' must be initialized by a constant expression}}
+    callTemplated<0>(); // expected-note {{in call to 'callTemplated()'}} expected-note@-6 {{subexpression not valid in a constant expression}}
 }
