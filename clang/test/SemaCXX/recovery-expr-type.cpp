2230df151ebd2ed8b6cf3b820f039a0e620a22e8
Haojian Wu <hokein.wu@gmail.com>
2020-04-29 13:53:24 +0200

    Fix static_assert diagnostic.

--- clang/test/SemaCXX/recovery-expr-type.cpp
+++ clang/test/SemaCXX/recovery-expr-type.cpp
@@ -12,3 +12,9 @@ void test() {
   int s = sizeof(make_indestructible()); // expected-error {{deleted}}
 }
 }
+
+namespace static_assert_diags {
+constexpr int foo() { return 1;} // expected-note {{candidate function not viable}}
+// verify the "not an integral constant expression" diagnostic is suppressed.
+static_assert(1 == foo(1), ""); // expected-error {{no matching function}}
+}
