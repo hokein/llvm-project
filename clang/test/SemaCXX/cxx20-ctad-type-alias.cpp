e5d7f1f05c2c058d8c491795985775c17dfcda27
Haojian Wu <hokein.wu@gmail.com>
2024-01-28 21:17:30 +0100

    Format test file.

--- clang/test/SemaCXX/cxx20-ctad-type-alias.cpp
+++ clang/test/SemaCXX/cxx20-ctad-type-alias.cpp
@@ -1,42 +1,47 @@
 // RUN: %clang_cc1 -fsyntax-only -Wno-c++11-narrowing -Wno-literal-conversion -std=c++20 -verify %s
 
 namespace test1 {
-template<typename T>
-struct Foo { T t;};
-template<typename U>
+template <typename T>
+struct Foo {
+  T t;
+};
+template <typename U>
 using Bar = Foo<U>;
 
 Bar s = {1};
-}
+}  // namespace test1
 
 namespace test2 {
-template<typename X, typename Y>
-struct XYpair { X x; Y y; };
+template <typename X, typename Y>
+struct XYpair {
+  X x;
+  Y y;
+};
 // A tricky explicit deduction guide that swapping X and Y.
-template<typename X, typename Y>
+template <typename X, typename Y>
 XYpair(X, Y) -> XYpair<Y, X>;
-template<typename U, typename V>
+template <typename U, typename V>
 using AliasXYpair = XYpair<U, V>;
 
-AliasXYpair xy = {1.1, 2}; // XYpair<int, double>
+AliasXYpair xy = {1.1, 2};  // XYpair<int, double>
 static_assert(__is_same(decltype(xy.x), int));
 static_assert(__is_same(decltype(xy.y), double));
-} // namespace test2
+}  // namespace test2
 
 namespace test3 {
-template<typename T, class>
+template <typename T, class>
 struct container {
   // test with default arguments.
-  container(T a , T b = T());
+  container(T a, T b = T());
 };
 
-template<class T> using vector = container<T, int>;
+template <class T>
+using vector = container<T, int>;
 vector v(0, 0);
-} // namespace test3
-
+}  // namespace test3
 
 namespace test4 {
-template<class T>
+template <class T>
 struct X {
   T t;
   X(T);
@@ -50,65 +55,73 @@ using AX = X<T>;
 
 AX s = {1};
 static_assert(__is_same(decltype(s.t), double));
-} // namespace test4
+}  // namespace test4
 
 namespace test5 {
-template<int B>
+template <int B>
 struct Foo {};
-template<int... C>
+
+template <int... C>
 using AF = Foo<1>;
-auto a = AF {};
-} // namespace test5
+auto a = AF{};
+}  // namespace test5
 
 namespace test6 {
-template<typename T, bool B = false>
+template <typename T, bool B = false>
 struct Foo {
   Foo(T);
 };
 // non-type template argument.
-template<typename T>
+template <typename T>
 using AF = Foo<T, 1>;
-AF b{0};//
-} // namespace test6
+
+AF b{0};  //
+}  // namespace test6
 
 namespace test7 {
-template<typename T>
-struct Foo { Foo(T); };
+template <typename T>
+struct Foo {
+  Foo(T);
+};
 
-template<typename U>
+template <typename U>
 using AF1 = Foo<U>;
-template<typename K>
-using AF2 = AF1<K>; // expected-note {{template is declared here}}
+template <typename K>
+using AF2 = AF1<K>;  // expected-note {{template is declared here}}
 // FIXME: support this case.
-AF2 b = 1; // expected-error {{alias template 'AF2' requires template arguments; argument deduction only allowed for class templates}}
-} // namespace test7
+AF2 b = 1;  // expected-error {{alias template 'AF2' requires template arguments; argument deduction only allowed for class templates}}
+}  // namespace test7
 
 namespace test8 {
-template<typename T, int N>
-struct Foo { Foo(T const (&)[N]); };
+template <typename T, int N>
+struct Foo {
+  Foo(T const (&)[N]);
+};
 
-template<typename X, int Y>
+template <typename X, int Y>
 using Bar = Foo<X, Y>;
 
 Bar s = {{1}};
-} // namespace test8
+}  // namespace test8
 
 namespace test9 {
-template<typename T, int N>
-struct Foo { Foo(T const (&)[N]); };
+template <typename T, int N>
+struct Foo {
+  Foo(T const (&)[N]);
+};
 
-template<typename X, int Y>
+template <typename X, int Y>
 using Bar = Foo<X, sizeof(X)>;
 
 // FIXME: should we reject this case? GCC rejects it, MSVC accepts it.
 Bar s = {{1}};
-} // namespace test9
+}  // namespace test9
 
 namespace test10 {
 template <typename T>
 struct Foo {
-    template <typename U>
-    Foo(U);
+  template <typename U>
+  Foo(U);
 };
 
 template <typename U>
@@ -116,5 +129,5 @@ Foo(U) -> Foo<U*>;
 
 template <typename K>
 using A = Foo<K>;
-A a(2); // Foo<int*>
-} // namespace test10
+A a(2);  // Foo<int*>
+}  // namespace test10
