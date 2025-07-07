8c1a61008458e9d5a3fba102d7531ea037b14795
Haojian Wu <hokein.wu@gmail.com>
2025-07-07 13:58:56 +0200

    Revert unintentional changes.

--- clang/tools/libclang/CIndex.cpp
+++ clang/tools/libclang/CIndex.cpp
@@ -7716,7 +7716,6 @@ static bool getTokens(ASTUnit *CXXUnit, SourceRange Range,
     CXToken CXTok;
 
     //   - Common fields
-    // CXTok.int_data[1] = Tok.getLocation().getRawEncoding();
     uint32_t TokLocRaw;
     if (!Tok.getLocation().getRawEncoding32(TokLocRaw))
       return false; // location is too big for libclang ABI
