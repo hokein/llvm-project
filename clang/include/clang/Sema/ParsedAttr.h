bae8211f98c4ed37a6b27d140002a2b5d7581c88
Haojian Wu <hokein.wu@gmail.com>
2025-07-14 13:30:45 +0200

    Set Attrs SmallVector<ParsedAttr *> size to 1

--- clang/include/clang/Sema/ParsedAttr.h
+++ clang/include/clang/Sema/ParsedAttr.h
@@ -678,7 +678,7 @@ class AttributePool {
   friend class AttributeFactory;
   friend class ParsedAttributes;
   AttributeFactory &Factory;
-  llvm::SmallVector<ParsedAttr *> Attrs;
+  llvm::SmallVector<ParsedAttr *, 1> Attrs;
 
   void *allocate(size_t size) {
     return Factory.allocate(size);
